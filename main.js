f3026fdf601d74a77805074b30280db74e4e9e50
husam saleh <husamsaleh.eng@gmail.com>
2022-09-19 10:55:46 +0300

    set up fitness website

---
+++ main.js
@@ -0,0 +1,173 @@
+$(document).ready(function () {
+    var itemsMainDiv = ('.MultiCarousel');
+    var itemsDiv = ('.MultiCarousel-inner');
+    var itemWidth = "";
+
+    $('.leftLst, .rightLst').click(function () {
+        var condition = $(this).hasClass("leftLst");
+        if (condition)
+            click(0, this);
+        else
+            click(1, this)
+    });
+
+    ResCarouselSize();
+
+
+
+
+    $(window).resize(function () {
+        ResCarouselSize();
+    });
+
+    //this function define the size of the items
+    function ResCarouselSize() {
+        var incno = 0;
+        var dataItems = ("data-items");
+        var itemClass = ('.item');
+        var id = 0;
+        var btnParentSb = '';
+        var itemsSplit = '';
+        var sampwidth = $(itemsMainDiv).width();
+        var bodyWidth = $('body').width();
+        $(itemsDiv).each(function () {
+            id = id + 1;
+            var itemNumbers = $(this).find(itemClass).length;
+            btnParentSb = $(this).parent().attr(dataItems);
+            itemsSplit = btnParentSb.split(',');
+            $(this).parent().attr("id", "MultiCarousel" + id);
+
+
+            if (bodyWidth >= 1200) {
+                incno = itemsSplit[3];
+                itemWidth = sampwidth / incno;
+            }
+            else if (bodyWidth >= 992) {
+                incno = itemsSplit[2];
+                itemWidth = sampwidth / incno;
+            }
+            else if (bodyWidth >= 768) {
+                incno = itemsSplit[1];
+                itemWidth = sampwidth / incno;
+            }
+            else {
+                incno = itemsSplit[0];
+                itemWidth = sampwidth / incno;
+            }
+            $(this).css({ 'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers });
+            $(this).find(itemClass).each(function () {
+                $(this).outerWidth(itemWidth);
+            });
+
+            $(".leftLst").addClass("over");
+            $(".rightLst").removeClass("over");
+
+        });
+    }
+
+
+    //this function used to move the items
+    function ResCarousel(e, el, s) {
+        var leftBtn = ('.leftLst');
+        var rightBtn = ('.rightLst');
+        var translateXval = '';
+        var divStyle = $(el + ' ' + itemsDiv).css('transform');
+        var values = divStyle.match(/-?[\d\.]+/g);
+        var xds = Math.abs(values[4]);
+        if (e == 0) {
+            translateXval = parseInt(xds) - parseInt(itemWidth * s);
+            $(el + ' ' + rightBtn).removeClass("over");
+
+            if (translateXval <= itemWidth / 2) {
+                translateXval = 0;
+                $(el + ' ' + leftBtn).addClass("over");
+            }
+        }
+        else if (e == 1) {
+            var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
+            translateXval = parseInt(xds) + parseInt(itemWidth * s);
+            $(el + ' ' + leftBtn).removeClass("over");
+
+            if (translateXval >= itemsCondition - itemWidth / 2) {
+                translateXval = itemsCondition;
+                $(el + ' ' + rightBtn).addClass("over");
+            }
+        }
+        $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
+    }
+
+    //It is used to get some elements from btn
+    function click(ell, ee) {
+        var Parent = "#" + $(ee).parent().attr("id");
+        var slide = $(Parent).attr("data-slide");
+        ResCarousel(ell, Parent, slide);
+    }
+
+});
+
+mybutton = document.getElementById("totoppls");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 560 || document.documentElement.scrollTop > 560) {
+        mybutton.style.display = "block";
+
+    } else {
+        mybutton.style.display = "none";
+
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+$(function () {
+    $('.scroll-down').click(function () {
+        $('html, body').animate({ scrollTop: $('section.ok').offset().top }, 'slow');
+        return false;
+    });
+});
+
+
+    //   window.onload = function () {
+    //       document.addEventListener("contextmenu", function (e) {
+    //           e.preventDefault();
+    //       }, false);
+    //       document.addEventListener("keydown", function (e) {
+    //           //document.onkeydown = function(e) {
+    //           // "I" key
+    //           if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+    //               disabledEvent(e);
+    //           }
+    //           // "J" key
+    //           if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+    //               disabledEvent(e);
+    //           }
+    //           // "S" key + macOS
+    //           if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+    //               disabledEvent(e);
+    //           }
+    //           // "U" key
+    //           if (e.ctrlKey && e.keyCode == 85) {
+    //               disabledEvent(e);
+    //           }
+    //           // "F12" key
+    //           if (event.keyCode == 123) {
+    //               disabledEvent(e);
+    //           }
+    //       }, false);
+    //       function disabledEvent(e) {
+    //           if (e.stopPropagation) {
+    //               e.stopPropagation();
+    //           } else if (window.event) {
+    //               window.event.cancelBubble = true;
+    //           }
+    //           e.preventDefault();
+    //           return false;
+    //       }
+    //   }
+
